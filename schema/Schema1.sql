982416c0de11a40cc7e848d844228f73dc59243c
Sitwat <sitwat.nadeem1@gmail.com>
2018-10-09 20:09:09 -0400

    fixed schema

--- schema/Schema1.sql
+++ schema/Schema1.sql
@@ -16,14 +16,14 @@ PRIMARY KEY (id)
 
 
 INSERT INTO symptoms (symptom, selected, disease_1, disease_2, disease_3, disease_4)
-VALUES  ('Fever', FALSE, 'Influenza', 'Rheumatoid arthritis', 'Shingles', 'Open cough');
+VALUES  ('Fever', FALSE, 'Influenza', 'Rheumatoid arthritis', 'Shingles', 'Open cough'),
 	( 'Headache', FALSE, 'Influenza', 'Shingles', 'Stroke', 'Malaria'),
         ('Vomiting', FALSE, 'Malaria', 'Norovirus', 'Anemia', 'Flu');
         
 INSERT INTO symptoms (symptom, selected, disease_1, disease_2)
 VALUES('Muscle Pain', FALSE, 'Influenza', 'Malaria'),
         ('Frequent Urination', FALSE, 'Gynecologic Cancers', 'Urinary track infection'),
-        ('Sneezing', FALSE, 'Allergy', 'Open cough');
+        ('Sneezing', FALSE, 'Allergy', 'Open cough'),
         ('Chills', FALSE, 'Flu', 'Urinary Tract Infection');
         
 INSERT INTO symptoms (symptom, selected, disease_1, disease_2, disease_3)
