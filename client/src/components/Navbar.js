ddd205e06f23be9367a65a663dd8bf6266e2ef9a
Yaraye <yaraye@gmail.com>
2018-10-11 00:11:56 -0400

    navbar updates

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,40 +1,41 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import '../App.css';
 
 
 const Navbar =props =>
- <nav  className='navbar navbar-default'>
-    <div className='container-fluid'>
-       <a className={
-        window.location.pathname === '/' 
-        ? 'active' :""
-        }
-        >
-        <Link to='/'>Login</Link>
-     </a>
-     <a className={
-        window.location.pathname === '/Home' 
-        ? 'active' :""
-        }
-        >
-        <Link to='/home'>Home</Link>
-     </a>
-     <a className={
-     window.location.pathname === '/search' 
-        ? 'active':''}
-        >
-        <Link to='/search'>
-        <div className='search-container'>
-            <form action=''>
-                <input type='text' placeholder='Search' name='search'/>
-                <button type="submit"><i className="fa fa-search"></i>icon</button>
-            </form>
+ <nav className="navbar navbar-default">
+<div className="container-fluid">
+  <div className="navbar-header">
+    <a className="navbar-brand">logo</a>
+  </div>
+  <ul className="nav navbar-nav">
+    <li className="active">  <a classNameName={
+      window.location.pathname === '/' 
+      ? 'active' :""
+      }
+      >
+      <Link to='/'>Login</Link>
+   </a></li>
+    <li><a classNameName={
+      window.location.pathname === '/Home' 
+      ? 'active' :""
+      }
+      >
+      <Link to='/home'>Home</Link>
+   </a></li>
+    <li><a classNameName={
+   window.location.pathname === '/search' 
+      ? 'active':''}
+      >
+      <Link to='/search'>Search</Link><form action=''>
+              <input type='text' placeholder='Search' name='search'/>
+              <button type="submit"><i classNameName="fa fa-search"></i>icon</button>
+          </form>
+</a></li>
+   
+  </ul>
 </div>
-</Link>
-</a>
-
- </div>
- </nav>
-
+</nav>
 
 export default Navbar;
