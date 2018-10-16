e83eced06ae501ffa6fc3747f0cab4ad24b76aff
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-10-16 16:08:07 -0400

    login component

---
+++ client/src/components/login/social-config.js
@@ -0,0 +1,16 @@
+const facebook = {
+    appId: '1931385593565712',
+    cookie: true,
+    xfbml: true,
+    version: 'v2.11',
+    scope: 'email'
+  };
+   
+  export const facebookConfig = facebook;
+
+  const google = {
+    client_id: 'YOUR GOOGLE APP ID GOES HERE',
+    scope: "https://www.googleapis.com/auth/plus.login"
+  };
+   
+  export const googleConfig = google;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React from 'react';
+import Sample from '../components/login/login';
+// import GoogleLogin from 'react-google-login';
+// import { GoogleLogin } from 'react-google-login';
+// import FacebookLogin from 'react-facebook-login';
+
 
 class Login extends React.Component{
 
     render(){
-        return <p>Login page</p>
+        return <div><p>Login page</p>
+        {/* <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    // onSuccess={responseGoogle}
+    // onFailure={responseGoogle}
+  />,
+    <FacebookLogin
+    appId="1931385593565712"
+    autoLoad={true}
+    fields="name,email,picture"
+    // onClick={componentClicked}
+    // callback={responseFacebook} />,
+    />, */}
+        <Sample/>
+        </div>
     }
 }
 
